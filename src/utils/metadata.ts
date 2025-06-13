e15a9c9c1f82194794a84325bf7ed3386bfbb65e
miyaparker <karen@cchub.africa>
2025-06-13 17:39:40 +0100

    implements assessments

---
+++ src/utils/metadata.ts
@@ -0,0 +1,9 @@
+const metadata = {
+    title: 'ElevateHR',
+    description: 'ElevateHR - AI-Powered Recruitment Platform',
+    icons: {
+      icon: '/favicon.svg',
+    },
+  }
+
+export default metadata; 
