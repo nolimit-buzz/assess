ef6272b856b4ad846bdba22fbb4ee3c98e33d5f7
miyaparker <karen@cchub.africa>
2025-06-24 17:22:44 +0100

    qa fixes

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,9 +1,10 @@
-const metadata = {
-    title: 'ElevateHR',
-    description: 'ElevateHR - AI-Powered Recruitment Platform',
-    icons: {
-      icon: '/favicon.svg',
-    },
-  }
-
-export default metadata; 
+export default {
+  host:"Assess.ng",
+  logo:"/images/logos/logo.svg",
+  favicon:"/images/logos/favicon.svg",
+  title:"Assess.ng",
+  description:"Assess.ng is a platform for assessing candidates for jobs.",
+  keywords:"Assess.ng,assess.ng, assessng, assessng.com, assessng.com",
+  author:"Assess.ng",
+  defaultBookingLink:"https://app.assess.ng/booking",
+}
